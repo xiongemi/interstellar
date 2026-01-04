0b40e6c1598635026ed9ef5f2aba2ea6b99b8f1e
Emily Xiong <xiongemi@gmail.com>
2026-01-03 19:19:33 -0500

    remove max workers
    run-many
    
    add --no-cache
    
    remove cyperss cache
    
    add jest clear cache
    
    remove eslint cache in nx.json
    
    add continue on error continue-on-error: true
    
    add next cache
    
    change pr

--- jest.preset.js
+++ jest.preset.js
@@ -1,9 +1,6 @@
 const nxPreset = require('@nx/jest/preset');
-const path = require('path');
 
 module.exports = {
   ...nxPreset,
   testEnvironment: 'jsdom',
-  maxWorkers: '50%',
-  cacheDirectory: path.join(__dirname, '.jest/cache'),
 };
