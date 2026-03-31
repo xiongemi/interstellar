9e2bb310ea9d9632861f88a27e314b871b9b0426
Emily Xiong <xiongemi@gmail.com>
2026-03-31 18:31:27 -0400

    remove maxWorkers in ci

--- jest.preset.js
+++ jest.preset.js
@@ -1,9 +1,11 @@
 const nxPreset = require('@nx/jest/preset');
 const path = require('path');
 
+const isCI = !!process.env.CI;
+
 module.exports = {
   ...nxPreset,
   testEnvironment: 'jsdom',
-  maxWorkers: '50%',
+  maxWorkers: isCI ? '100%': '50%',
   cacheDirectory: path.join(__dirname, '.jest/cache'),
 };
