34d66a5f8c8161380d77b6748d7b71ef9136d547
Emily Xiong <xiongemi@gmail.com>
2026-04-12 14:26:54 -0400

    add more e2e tests

---
+++ apps/ticket-booking-e2e/playwright.config.ts
@@ -0,0 +1,34 @@
+import { defineConfig, devices } from '@playwright/test';
+import { nxE2EPreset } from '@nx/playwright/preset';
+import { workspaceRoot } from '@nx/devkit';
+
+const baseURL = process.env['BASE_URL'] || 'http://localhost:3004';
+
+export default defineConfig({
+  ...nxE2EPreset(__filename, { testDir: './e2e' }),
+  use: {
+    baseURL,
+    trace: 'on-first-retry',
+  },
+  webServer: {
+    command: 'nx run ticket-booking:start',
+    url: 'http://localhost:3004',
+    reuseExistingServer: !process.env.CI,
+    cwd: workspaceRoot,
+    timeout: process.env.CI ? 180_000 : 90_000,
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
+  ],
+});
---
+++ apps/warp-drive-manager-e2e/eslint.config.mjs
@@ -0,0 +1,11 @@
+import playwright from 'eslint-plugin-playwright';
+import baseConfig from '../../eslint.config.mjs';
+
+export default [
+  playwright.configs['flat/recommended'],
+  ...baseConfig,
+  {
+    files: ['**/*.ts', '**/*.js'],
+    rules: {},
+  },
+];
